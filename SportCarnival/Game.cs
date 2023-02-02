1da2732edee2fe5fdfc42d6250618b377d36f328
Himanshu Gupta <himanshu.gupta.m@intimetec.com>
2023-02-02 12:52:07 +0530

    Deserializer Json to Object

---
+++ SportCarnival/Game.cs
@@ -0,0 +1,35 @@
+﻿using SportCarnival.Enums;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SportCarnival
+{
+    public class Game
+    {
+        public GameType GameType { get; set; }
+
+        public List<Player> Players { get; set; }
+    }
+
+    public class Player
+    {
+        public int PlayerId { get; set; }
+        public string Name { get; set; }
+    }
+
+    public class TeamList
+    {
+        public List<Team> Teams { get; set; }
+        public int Total { get; set; }
+    }
+
+    public class Team : Game
+    {
+        public int TeamId { get; set; }
+
+        public string TeamName { get; set; }
+    }
+}
---
+++ SportCarnival/TeamMethods.cs
@@ -0,0 +1,24 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SportCarnival
+{
+    public class TeamMethods
+    {
+        private Game game = JsonConvert.DeserializeObject<Game>(File.ReadAllText(@"TeamInput.json"));
+
+        public TeamList CreateTeam(Game game)
+        {
+            switch (game.GameType)
+            {
+                case 1:
+            }
+            return null;
+        }
+    }
+}
